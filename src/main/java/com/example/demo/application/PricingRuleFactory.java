18be6b4ca6dd8b74e58a8c56a180bd5556c4281d
Xi Qi <xqi@thoughtworks.com>
2022-04-17 20:32:14 +0800

    read pricing config from properties

--- src/main/java/com/example/demo/application/PricingRuleFactory.java
+++ src/main/java/com/example/demo/application/PricingRuleFactory.java
@@ -3,15 +3,22 @@ package com.example.demo.application;
 import com.example.demo.domain.pricing.CompositePricingRule;
 import com.example.demo.domain.pricing.DiscountPricingRule;
 import com.example.demo.domain.pricing.PricingRule;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
 @Configuration
+@ConfigurationProperties("demo.pricing")
+@Setter
 public class PricingRuleFactory {
+    private int amount;
+    private float discount;
+
     @Bean
     PricingRule pricingRule() {
-        return new CompositePricingRule(List.of(new DiscountPricingRule(2, 0.8f)));
+        return new CompositePricingRule(List.of(new DiscountPricingRule(amount, discount)));
     }
 }
